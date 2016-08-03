b8634ed5def76b82031b5e1bb40fcc10cdb1565a
Sudarsan <sudar.theone@gmail.com>
2016-08-03 16:48:24 +0530

    added standard deviation and mean calculations

---
+++ mathOps.go
@@ -0,0 +1,29 @@
+package gBay
+
+import (
+		"math"
+		"fmt"
+	)
+
+
+func Sum(dataset []float64) float64{
+	var sum float64
+	for _, vals := range dataset{
+		sum += vals
+	}
+	return sum
+}
+
+func Mean(dataset []float64) float64{
+	return Sum(dataset) / float64(len(dataset))
+}
+
+func stdDev(dataset []float64) float64{
+	var variance float64
+	for _ , values := range dataset{
+		variance+=(math.Pow((Mean(dataset) - values),2))/float64(len(dataset))
+	}
+	return math.Sqrt(variance)
+
+}
+
